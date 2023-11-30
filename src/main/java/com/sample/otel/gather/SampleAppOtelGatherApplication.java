6afa9a337c6f721dc8189b55f7127107b8e31a66
Seth Levine <sethlevi@amazon.com>
2023-11-30 14:01:48 -0500

    start.spring.io

---
+++ src/main/java/com/sample/otel/gather/SampleAppOtelGatherApplication.java
@@ -0,0 +1,13 @@
+package com.sample.otel.gather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAppOtelGatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAppOtelGatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/otel/gather/SampleAppOtelGatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.otel.gather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAppOtelGatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
